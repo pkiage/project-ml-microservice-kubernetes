19e3c8b12f9424466b1f0ec3ec702b0621e3ff2c
pkiage <paul.r.kiage@gmail.com>
2022-07-24 10:49:37 +0300

    make run_docker.sh more descriptive
    - add comments
    - longer form tags
    - add references

--- run_docker.sh
+++ run_docker.sh
@@ -5,13 +5,15 @@
 # https://www.docker.com/wp-content/uploads/2022/03/docker-cheat-sheet.pdf
 # https://docs.docker.com/engine/reference/commandline/build/
 # https://docs.docker.com/engine/reference/commandline/docker/
+# https://docs.docker.com/engine/reference/commandline/run/
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-# Name and optionally a tag in the 'name:tag' format
-docker build -t mlapi .
+
+# name and optionally a tag in the 'name:tag' format
+docker build --tag mlapi .
 
 # Step 2: 
 # List docker images
@@ -21,4 +23,6 @@ docker image ls
 # Run flask app
 # 8000 from make_predictions.sh
 # 80 from Dockerfile
-docker run -p 8000:80 mlapi
+
+# publish a container's port(s) to the host
+docker run --publish 8000:80 mlapi
